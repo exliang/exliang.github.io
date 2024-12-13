938886709f54bf6bf62a18ce83c465a77dbc44f0
Emily Liang <exliang@uci.edu>
2024-12-12 20:55:50 -0800

    Education in About section

--- design.js
+++ design.js
@@ -7,14 +7,17 @@ navLinks.forEach(link => {
         if (link.classList.contains('home') || link.classList.contains('name')) {
             window.location.href = 'home.html';
         }
-		else if (link.classList.contains('projects')) {
-            window.location.href = 'projects.html';
+		else if (link.classList.contains('portfolio')) {
+            window.location.href = 'portfolio.html';
         }
 		else if (link.classList.contains('contact')) {
             window.location.href = 'contact.html';
         }
 		else if (link.classList.contains('about')) {
             window.location.href = 'about.html';
+        }
+		else if (link.classList.contains('experience')) {
+            window.location.href = 'experience.html';
         }
     });
 });
