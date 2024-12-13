6e540931a105f88854f2826d4d04f670af003a63
Emily Liang <exliang@uci.edu>
2024-12-12 20:08:05 -0800

    home html pg & contact pg

---
+++ design.js
@@ -0,0 +1,17 @@
+
+const navLinks = document.querySelectorAll('.nav-link');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', event => {   
+		// event.preventDefault();     
+        if (link.classList.contains('home') || link.classList.contains('name')) {
+            window.location.href = 'home.html';
+        }
+		else if (link.classList.contains('projects')) {
+            window.location.href = 'projects.html';
+        }
+		else if (link.classList.contains('contact')) {
+            window.location.href = 'contact.html';
+        }
+    });
+});
